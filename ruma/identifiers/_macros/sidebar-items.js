4c4d20548111f56064da4d98ef5cb85eb38e520c
jplatte <jplatte@users.noreply.github.com>
2021-04-06 17:32:56 +0000

    Deploying to docs from @ ruma/ruma@bd61e97b96ba1400719c529ad7d826df3cbcdfe7 🚀

--- ruma/identifiers/_macros/sidebar-items.js
+++
@@ -1,1 +0,0 @@
-initSidebarItems({"macro":[["device_key_id",""],["event_id",""],["mxc_uri",""],["room_alias_id",""],["room_id",""],["room_version_id",""],["server_name",""],["server_signing_key_id",""],["user_id",""]]});
