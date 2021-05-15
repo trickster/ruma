c04f96395fcee5d407c2a2945df63d46c5bb9f3e
jplatte <jplatte@users.noreply.github.com>
2021-05-15 13:24:07 +0000

    Deploying to docs from @ ruma/ruma@89b169faf37e2e0170484bdeefa7c1f68fd076e9 🚀

--- implementors/ruma_events/trait.RoomEventContent.js
+++
@@ -1,3 +0,0 @@
-(function() {var implementors = {};
-implementors["ruma_events"] = [];
-if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
