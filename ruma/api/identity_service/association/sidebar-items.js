50529b67c5a5ab2727b0d16be0c21f07860b04a2
jplatte <jplatte@users.noreply.github.com>
2021-06-08 12:47:56 +0000

    Deploying to docs from @ ruma/ruma@88f8dc6b07a2bdba9e8e3d120c202b32a1c45bc2 🚀

--- ruma/api/identity_service/association/sidebar-items.js
+++ ruma/api/identity_service/association/sidebar-items.js
@@ -1,1 +1,1 @@
-initSidebarItems({"mod":[["email","Endpoints to create an association between a Matrix ID and an email ID on the identity server."],["msisdn","Endpoints to create association between a phone number and a Matrix ID on the identity server."]]});
+initSidebarItems({"mod":[["check_3pid_validity","Endpoint to determine the validity of a 3PID."],["email","Endpoints to create an association between a Matrix ID and an email ID on the identity server."],["msisdn","Endpoints to create association between a phone number and a Matrix ID on the identity server."]]});
---
+++ ruma_identity_service_api/association/check_3pid_validity/sidebar-items.js
@@ -0,0 +1,1 @@
+initSidebarItems({"mod":[["v2","GET /_matrix/identity/v2/3pid/getValidated3pid"]]});
